ffd2d0adce97599cb6a222f89854d7a0fd3c54ab
Ryan Jordan <ryjordan@gmail.com>
2023-08-18 09:36:58 -0500

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'GitLex',
+    description: `Imagine being able to generate a commit message with the click of a button.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://github.com/krjordan/GitLexJs',
+  },
+]
+
+export default projectsData
